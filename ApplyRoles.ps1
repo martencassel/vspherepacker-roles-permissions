57df3eafe78d73020251a845c0d7889d3cbff2ba
Marten Cassel <marten.cassel@gmail.com>
2024-08-15 09:08:34 +0200

    more

--- ApplyRoles.ps1
+++ ApplyRoles.ps1
@@ -19,6 +19,37 @@ param($FileName = $(throw "FileName is required"))
 Set-PSDebug -Trace 0
 Set-StrictMode -Version Latest -ErrorAction Stop
 
+
+Function VerifyPrereqs() {
+    $ErrorList = @()
+    if($FileName -eq $null) {
+        $ErrorList += "FileName is required"
+    }
+    $RequiredCmdlets = @("Get-VMHost", "Get-VIRole", "Get-VIPrivilege", "New-VIRole")
+    for ($i = 0; $i -lt $RequiredCmdlets.Length; $i++) {
+        if (-not (Get-Command -Name $RequiredCmdlets[$i] -ErrorAction SilentlyContinue)) {
+            $ErrorList += "Cmdlet: $($RequiredCmdlets[$i]) not found"
+        }
+    }
+    $VCenterConnected = $true
+    try {
+        $vmHost = Get-VMHost -ErrorAction Stop
+        if ($vmHost) {
+            $VCenterConnected = $true
+        } else {
+            $VCenterConnected = $false
+        }
+    } catch {
+        $VCenterConnected = $false
+    }
+    if($VCenterConnected -eq $false) {
+        $ErrorList += "Not connected to vSphere server"
+        return $ErrorList
+    }
+    return $ErrorList
+}
+
+
 # Read the permissions from the file (yaml file)
 function ImportVIPermissions {
     param($FileName)
@@ -118,4 +149,12 @@ function Main() {
     Write-Host "Permissions applied successfully" -ForegroundColor DarkGreen
     Get-VIPermission|Sort-Object
 }
+
+
+$Errors = VerifyPrereqs
+if ($null -ne $Errors) {
+    Write-Error ("{0}" -f $Errors)
+    exit
+}
 Main
+
--- ImportRoles.ps1
+++ ImportRoles.ps1
@@ -2,7 +2,44 @@ param($FileName = $(throw "FileName is required"))
 Set-PSDebug -Trace 0
 Set-StrictMode -Version Latest -ErrorAction Stop
 
+Function VerifyPrereqs() {
+    $ErrorList = @()
+    if($FileName -eq $null) {
+        $ErrorList += "FileName is required"
+    }
+    $RequiredCmdlets = @("Get-VMHost", "Get-VIRole", "Get-VIPrivilege", "New-VIRole")
+    for ($i = 0; $i -lt $RequiredCmdlets.Length; $i++) {
+        if (-not (Get-Command -Name $RequiredCmdlets[$i] -ErrorAction SilentlyContinue)) {
+            $ErrorList += "Cmdlet: $($RequiredCmdlets[$i]) not found"
+        }
+    }
+    $VCenterConnected = $true
+    try {
+        $vmHost = Get-VMHost -ErrorAction Stop
+        if ($vmHost) {
+            $VCenterConnected = $true
+        } else {
+            $VCenterConnected = $false
+        }
+    } catch {
+        $VCenterConnected = $false
+    }
+    if($VCenterConnected -eq $false) {
+        $ErrorList += "Not connected to vSphere server"
+        return $ErrorList
+    }
+    return $ErrorList
+}
+
 Function Main() {
+    if (-not (Test-Path $FileName)) {
+        Write-Error "File: $FileName not found"
+        exit
+    }
+    if($FileName -eq $null) {
+        Write-Error "FileName is required"
+        exit
+    }
     $RoleDefinitions = cat $FileName|yq -ojson|ConvertFrom-Json|Select-Object roles -ExpandProperty roles
 
     foreach($RoleDef in $RoleDefinitions) {
@@ -29,6 +66,9 @@ Function Main() {
     }
 }
 
-
+$Errors = VerifyPrereqs
+if ($null -ne $Errors) {
+    Write-Error ("{0}" -f $Errors)
+    exit
+}
 Main
-
